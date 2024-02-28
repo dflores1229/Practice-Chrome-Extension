05a38fe6a0d7fad24914cab4525db4a9c894db6a
dflores1229 <d.flores1229@gmail.com>
2024-02-28 10:39:21 -0800

    fetch data from API

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //define fetch data funct
 async function fetchData(){
-    const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data")
+    //fetch data from website API
+    const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
+    //store JSON data
     const record = await res.json();
 
-    document.getElementById("date").innerHTML = record.data[0].date
-    document.getElementById("areaName").innerHTML = record.data[0].areaName
-    document.getElementById("latestBy").innerHTML = record.data[0].latestBy
-    document.getElementById("deathNew").innerHTML = record.data[0].deathNew
+    document.getElementById("date").innerHTML = record.data[0].date;
+    document.getElementById("areaName").innerHTML = record.data[0].areaName;
+    document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
+    document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
     
 }
-
+fetchData();
